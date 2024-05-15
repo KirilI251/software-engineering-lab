dd0f0b2a88465817da081a71a5f3c2e94e3ca8fc
Kirill251 <pidgirnom@gmail.com>
2024-05-16 00:42:23 +0300

    Done.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'My laboratory work №2',
   tagline: 'Pidgirny Kirill, KP-32',
   favicon: 'img/favicon.ico',
 
@@ -66,7 +66,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'My laboratory work №2',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
